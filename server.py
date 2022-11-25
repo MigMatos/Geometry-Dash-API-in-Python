5e303131efc6cb88b27a9120acb79a41ac7af33a
MigMatos <87149085+MigMatos@users.noreply.github.com>
2022-11-24 20:26:54 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 import asyncio
-from api.gd_target_api import get_search_profile,get_info_profile
+from api.gd_target_api import get_search_profile,get_info_profile,search_level
 #Search profile str
 json_data = asyncio.run(get_search_profile("obeygdbot","robtop"))
 print(json_data)
 #Search profile id account
 json_data = asyncio.run(get_info_profile("17255842","robtop"))
 print(json_data)
+
+#EXPERIMENTAL SEARCH LEVEL (BROKEN JSON)
+#data = asyncio.run(search_level("nine circles"))
+#print(data)
