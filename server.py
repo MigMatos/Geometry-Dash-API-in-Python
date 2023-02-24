babc652ff05fba58bd19de48778526ac7291ba8e
MigMatos <87149085+MigMatos@users.noreply.github.com>
2023-02-24 01:35:03 -0600

    Update server.py

--- server.py
+++ server.py
@@ -8,8 +8,18 @@ print(json_data)
 #First data is ID Account and 'robtop' is the GDPS (you can select another GDPS custom)
 json_data = asyncio.run(get_info_profile("17255842","robtop"))
 print(json_data)
-
 #Search levels
-#First data is STR or IDlevel and 'robtop' is the GDPS (you can select another GDPS custom)
-data = asyncio.run(search_level("nine circles","robtop"))
+#'robtop' is the GDPS (you can select another GDPS custom)
+#Query is the query in search level
+#Page is the page selected
+data = asyncio.run(search_level(server="robtop",query='"str":"Nine Circles"',page=0))
 print(data)
+#Send messages
+#'robtop' is the GDPS (you can select another GDPS custom)
+#title is the title of message
+#message is the body of message
+#accountIDtarget is the IDaccount is you sending message
+data = asyncio.run(send_message(server="robtop",title="Hi",message="Hello world",accountIDtarget=16))
+#return 1 if message send is sucess, 0 is error
+print(data)
+
