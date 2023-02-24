21c8cca52e8dff5b5c0647b4e7b633485837e79f
MigMatos <87149085+MigMatos@users.noreply.github.com>
2023-02-24 01:29:45 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 import asyncio
 from api.gd_target_api import get_search_profile,get_info_profile,search_level
 #Search profile str
+#First data is username and 'robtop' is the GDPS (you can select another GDPS custom)
 json_data = asyncio.run(get_search_profile("obeygdbot","robtop"))
 print(json_data)
 #Search profile id account
+#First data is ID Account and 'robtop' is the GDPS (you can select another GDPS custom)
 json_data = asyncio.run(get_info_profile("17255842","robtop"))
 print(json_data)
 
-#EXPERIMENTAL SEARCH LEVEL (BROKEN JSON)
-#data = asyncio.run(search_level("nine circles"))
-#print(data)
+#Search levels
+#First data is STR or IDlevel and 'robtop' is the GDPS (you can select another GDPS custom)
+data = asyncio.run(search_level("nine circles","robtop"))
+print(data)
